b0516136bc8dadc9b377e55357c3932c54bcff60
modalatarunkumar <modalatarunkumar@gmail.com>
2023-06-06 19:46:57 +0530

    06-06-2023

---
+++ javascript/js/06-06-2023(1).js
@@ -0,0 +1,14 @@
+var arr=[[1,2],[3,4],[5,6,7]];
+var product=1;
+for(let i=0;i<arr.length;i++)
+for(let j=0;j<arr[i].length;j++)
+product*=arr[i][j];
+console.log("Product "+product);
+
+console.log("Random :"+Math.floor(Math.random()*20));
+const arguments=process.argv;
+console.log(arguments)
+console.log(parseInt('10011',2));
+var a=0;
+var res=(a>0)?'positive':(a<0)?'negative':"zero";
+console.log(res);
