0bdb7b0498a2b66498e29decf69d2ec794304617
modalatarunkumar <modalatarunkumar@gmail.com>
2023-06-27 18:44:29 +0530

    27-06-2023

---
+++ javascript/js/25-06-2023(1).js
@@ -0,0 +1,15 @@
+class fun{
+    constructor(){
+        this.a=20;
+        console.log(this.a);
+    }
+    funn(){
+        console.log("hai");    
+    }
+}
+const obj=new fun();
+obj.funn();
+console.log(10<20<30);
+
+console.log(30>20>10);
+
---
+++ javascript/js/25-06-2023(2).js
@@ -0,0 +1,12 @@
+a=50;
+console.log(a);
+
+var a;
+
+
+var b=20;
+function fun(){
+    console.log(b);
+    var b=150;
+}
+fun();
